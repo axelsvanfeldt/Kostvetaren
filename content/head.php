2abf2632cfbf586d1493dc6e753f3566a33f5243
axel <axel.svanfeldt@gmail.com>
2019-04-27 02:05:07 +0200

    Added head.php to content

---
+++ content/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <title><?php echo $config["full_name"];?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="näringsämnen,näringsinnehåll,livesmedelsinnehåll,livsmedel,mat,Livsmedelsverket,hälsa,matmedveten">
+    <meta name="description" content="<?php echo $config["full_name"];?> hjälper dig att enkelt ta reda på näringsämnen i din mat. Sök bland cirka 2100 livsmedel och maträtter!">
+    <meta name="author" content="Axel Svanfeldt">
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+    <link href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
