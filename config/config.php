293cba3a9612fe51c83115cbc6ca19664e34b674
axel <axel.svanfeldt@gmail.com>
2019-04-29 00:03:23 +0200

    Changed format of database settings

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
 $config = array(
     "full_name" => 'Kostvetaren',
-    "database_connection" => new PDO("mysql:host=localhost;dbname=kostvetaren", "root", "", array(
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    )),
-    
+    "database" => array(
+        "host" => 'localhost',
+        "database" => 'kostvetaren',
+        "username" => 'root',
+        "password" => ''
+    )
 );
 ?>
